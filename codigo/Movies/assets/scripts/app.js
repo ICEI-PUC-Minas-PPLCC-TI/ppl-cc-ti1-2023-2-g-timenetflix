009080f2213078ec34c736c3685db2bde76081a5
Adelino <compianitone@gmail.com>
2023-11-13 00:00:24 -0300

    filmes de atores

--- codigo/Movies/assets/scripts/app.js
+++ codigo/Movies/assets/scripts/app.js
@@ -1,47 +1,45 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const atoresRecomendadosElement = document.getElementById("atores-recomendados");
-  
-    // Função para obter a lista de atores com base nos filmes
-    async function obterAtoresRecomendados() {
-      try {
-        const response = await fetch("http://localhost:3000/filmes");
-        const filmes = await response.json();
-  
-        const atores = filmes.reduce((atores, filme) => {
-          filme.atores.forEach((ator) => {
-            if (!atores.includes(ator)) {
-              atores.push(ator);
-            }
-          });
-          return atores;
-        }, []);
-  
-        return atores;
-      } catch (error) {
-        console.error("Erro ao obter dados do servidor:", error);
-      }
-    }
-  
-    // Função para exibir atores recomendados na página
-    async function exibirAtoresRecomendados() {
-      const atoresRecomendados = await obterAtoresRecomendados();
-  
-      if (atoresRecomendados.length > 0) {
-        const listaAtores = document.createElement("ul");
-  
-        atoresRecomendados.forEach((ator) => {
-          const itemLista = document.createElement("li");
-          itemLista.textContent = ator;
-          listaAtores.appendChild(itemLista);
+const apiUrl = 'https://jsonserver.adelinocompiani.repl.co/atore';  
+
+
+let currentActorIndex = 0;
+
+function updateActorInfo() {
+    const actorInfo = actors[currentActorIndex];
+    $('#actor-info').html(`
+        <h3>${actorInfo.name}</h3>
+    `);
+}
+
+function updateMovieList(actorName) {
+    fetch(`${apiUrl}/movies/${actorName}`)
+        .then(response => response.json())
+        .then(data => {
+            const movieList = data.map(movie => `<li>${movie.title}</li>`).join('');
+            $('#movie-list').html(`<ul>${movieList}</ul>`);
+        })
+        .catch(error => {
+            console.error('Error fetching movie data:', error);
         });
-  
-        atoresRecomendadosElement.appendChild(listaAtores);
-      } else {
-        atoresRecomendadosElement.textContent = "Nenhum ator recomendado.";
-      }
-    }
-  
-    // Chama a função para exibir atores recomendados
-    exibirAtoresRecomendados();
-  });
-  
+}
+
+$(document).ready(function () {
+    // Assuming you've loaded actors from the server or hard-coded them
+    const actors = [
+        { name: 'Brad Pitt' },
+        // Add more actors as needed
+    ];
+
+    // Initial page load
+    updateActorInfo();
+    updateMovieList(actors[currentActorIndex].name);
+
+    // Event listener for Next Actor button
+    $('#next-actor').click(function () {
+        currentActorIndex = (currentActorIndex + 1) % actors.length;
+        updateActorInfo();
+        updateMovieList(actors[currentActorIndex].name);
+    });
+});
+
+
+
